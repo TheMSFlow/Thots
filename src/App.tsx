3b7e26718f12a02b3b9573a763014d3d1683b094
Michael <themichaelsteve@gmail.com>
2025-08-07 04:38:58 +0100

    Git ignore updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-row justify-center items-center h-screen bg-blue-100">
-        <h1 className="text-blue-500">
+      <div className="flex flex-row justify-center items-center h-screen bg-blue-950">
+        <h1 className="text-blue-200">
           Thots
         </h1>
       </div>
