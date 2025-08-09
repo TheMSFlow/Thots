9f0f4c210de3e0709ff902f6920da1b9ff4873c2
Michael <themichaelsteve@gmail.com>
2025-08-09 01:57:59 +0100

    Fixed Bug with rendering single post

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -7,13 +7,26 @@ import PostCardSkeleton from "../components/PostCardSkeleton";
 import Header from "../components/Header";
 import Spacer from "../components/blocks/Spacer";
 import Toast from "../components/blocks/Toast";
+import { useEffect } from "react";
+import { useApolloClient } from "@apollo/client";
 
 const PostPage = () => {
+  const client = useApolloClient();
+
   const { id } = useParams<{ id: string }>();
 
   const { data, loading, error } = useQuery(GET_POST_BY_ID, {
     variables: { post_id: id },
+    fetchPolicy: "network-only",
   });
+
+  useEffect(() => {
+    return () => {
+      client.cache.evict({ fieldName: "postsCollection" });
+      client.cache.gc();
+    };
+  }, []);
+  
 
   const post = data?.postsCollection?.edges?.[0]?.node;
 
@@ -21,18 +34,18 @@ const PostPage = () => {
 
   }
 
+  // if (loading) return <PostCardSkeleton />
   if (error) return <p className="flex justify-center items-center h-screen px-4 text-center">Error: {error.message}</p>;
 
-  if (!post) return <p className="flex justify-center items-center h-screen px-4 text-center">Post not found.</p>;
+  if (!post && !loading) return <p className="flex justify-center items-center h-screen px-4 text-center">Post not found.</p>;
 
   return (
     <>
       <Header />
       <Spacer />
       <div className="flex flex-col justify-start items-center gap-2 max-w-full lg:max-w-[40rem] lg:p-12 lg:bg-[#F8F8F8] lg:rounded-2xl mx-auto mb-8 lg:mb-20">
-        <div className="w-full flex flex-col gap-8">
-            {loading && <PostCardSkeleton />}
-            <PostCard {...post} />
+        <div className="w-full flex flex-col">
+            {loading ? <PostCardSkeleton /> : <PostCard {...post} />}
         </div>
       </div>
       <Toast />
